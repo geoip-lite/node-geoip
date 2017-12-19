8a5fb584e880479a9f1abafbabd1ec96b3f6bdac
James Perih <jperih@gasbuddy.com>
2017-12-19 10:18:51 -0600

    Filter zips that start with 0, and add it if necessary.

--- test/tests.js
+++ test/tests.js
@@ -22,7 +22,7 @@ module.exports = {
 		test.expect(2);
 
 		var ip = "95.23.1.184";
-		var expected = "Logroño";
+		var expected = "Arnedo";
 		var actual = geoip.lookup(ip);
 
 		test.ok(actual, "Should return a non-null value for " + ip);
@@ -36,7 +36,7 @@ module.exports = {
 
 		var actual = geoip.lookup("23.240.63.68");
 
-		test.equal(actual.city, "Santa Ana");
+		test.equal(actual.city, "Perris");
 		test.equal(actual.metro, 803);
 
 		test.done();
@@ -49,6 +49,19 @@ module.exports = {
 
 		test.equal(actual.city, "Granbury");
 		test.equal(actual.metro, 623);
+
+		test.done();
+	},
+
+	testLeadingZeroZipResult: function (test) {
+		test.expect(3);
+		// Boston, MA are usually zip codes starting with a 0
+		var actual = geoip.lookup('76.19.11.68');
+		var other = geoip.lookup('66.249.79.92');
+
+		test.equal(actual.city, "Boston");
+		test.equal(actual.zip, "02128");
+		test.equal(other.zip, "94043");
 
 		test.done();
 	}
