560c58afc8b23b9d253913cb0fd9fff41da7c56a
Sefinek <contact@sefinek.net>
2025-09-07 19:05:17 +0200

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,20 @@
-declare module 'geoip-lite2' {
-    export const cmp: number | null;
-
-    interface GeoIp2Location {
-        range: [number | null, number | null];
-        country: string;
-        region: string;
-        eu: '0' | '1';
-        timezone: string;
-        city: string;
-        ll: [number | null, number | null];
-        metro: number;
-        area: number;
-    }
-
-    function lookup(ip: string | number): GeoIp2Location | null;
-    function pretty(n: string | number | any[]): string;
-
-    function reloadDataSync(): void;
-    function reloadData(callback: (err?: Error | null) => void): Promise<void>;
-    function startWatchingDataUpdate(callback: () => void): void;
-    function stopWatchingDataUpdate(): void;
-    function clear(): void;
-
-    export {
-        lookup,
-        pretty,
-        reloadDataSync,
-        reloadData,
-        startWatchingDataUpdate,
-        stopWatchingDataUpdate,
-        clear
-    };
+interface GeoIp2Location {
+    range: [number | null, number | null];
+    country: string;
+    region: string;
+    eu: '0' | '1';
+    timezone: string;
+    city: string;
+    ll: [number | null, number | null];
+    metro: number;
+    area: number;
 }
+
+export function lookup(ip: string | number): GeoIp2Location | null;
+export function pretty(n: string | number | any[]): string;
+export function reloadDataSync(): void;
+export function reloadData(callback?: (err?: Error | null) => void): Promise<void>;
+export function startWatchingDataUpdate(callback: () => void): void;
+export function stopWatchingDataUpdate(): void;
+export function clear(): void;
+export const version: string;
