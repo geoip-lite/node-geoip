7f681b96b9196ca4879af123247f0929dfe7e623
Sefinek <contact@sefinek.net>
2025-12-10 17:07:34 +0100

    Fix tests

--- test/index.test.js
+++ test/index.test.js
@@ -44,10 +44,10 @@ describe('GeoIP2', () => {
 			expect(actual.country).toBe('JP');
 			expect(actual.eu).toBe('0');
 			expect(actual.timezone).toBe('Asia/Tokyo');
-			expect(actual.city).toBe('Ueda');
+			expect(actual.city).toBe('Kawasaki');
 			expect(actual.ll).toBeTruthy();
 			expect(actual.metro).toBe(0);
-			expect(actual.area).toBe(500);
+			expect(actual.area).toBe(200);
 		});
 
 		it('should match data for IPv4 - RU', () => {
@@ -70,7 +70,7 @@ describe('GeoIP2', () => {
 			expect(actual.region).toBe('06');
 			expect(actual.eu).toBe('1');
 			expect(actual.timezone).toBe('Europe/Warsaw');
-			expect(actual.city).toBe('Konopnica');
+			expect(actual.city).toBe('Lublin');
 			expect(actual.ll).toBeTruthy();
 			expect(actual.metro).toBe(0);
 			expect(actual.area).toBe(100);
